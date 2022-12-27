8bcd35248fa12b62e905b83ee88a569ac4f3d4a6
Pietro Giordano <giopi87@gmail.com>
2022-12-27 09:51:43 +0100

    bonus 2 completato

--- bonus2/js/scripts.js
+++ bonus2/js/scripts.js
@@ -13,13 +13,12 @@ const images = [
 ];
 
 const carouselSlide = document.querySelector('.carousel');
-const thumbSlide = document.querySelector('.thumbnails');
 
 for (let i = 0; i < images.length; i++) {
 
       console.log(images[i]);
       carouselSlide.innerHTML += `<div class="slide"><img src="${images[i]}"></div>`;
-      thumbSlide.innerHTML += `<div class="t-slide"><img src="${images[i]}"></div>`;
+      document.querySelector('.thumbnails').innerHTML += `<div class="t-slide"><img src="${images[i]}"><div class="t-over"></div></div>`;
 
 }
 
@@ -31,6 +30,9 @@ const allThumb = document.querySelectorAll('.t-slide');
 console.log(allThumb);
 allThumb[0].classList.add('t-selected');
 
+const overlay = document.querySelectorAll('.t-over');
+overlay[0].classList.add('t-none');
+
 const previous = document.querySelector('.previous');
 const next = document.querySelector('.next');
 let current = 0;
@@ -41,16 +43,22 @@ next.addEventListener('click',
 
             console.log('Cliccato su next');
             allSlides[current].classList.remove('selected');
+            allThumb[current].classList.remove('t-selected');
+            overlay[current].classList.remove('t-none');
             
             if (current == allSlides.length - 1) {
                   
                   current = 0;
                   allSlides[current].classList.add('selected');
+                  allThumb[current].classList.add('t-selected');
+                  overlay[current].classList.add('t-none');
       
             } else {
 
                   current++;
                   allSlides[current].classList.add('selected');
+                  allThumb[current].classList.add('t-selected');
+                  overlay[current].classList.add('t-none');
 
             }
       
@@ -64,16 +72,22 @@ previous.addEventListener('click',
 
             console.log('Cliccato su previous');
             allSlides[current].classList.remove('selected');
+            allThumb[current].classList.remove('t-selected');
+            overlay[current].classList.remove('t-none');
 
             if (current == 0) {
 
                   current = allSlides.length - 1;
                   allSlides[current].classList.add('selected');
+                  allThumb[current].classList.add('t-selected');
+                  overlay[current].classList.add('t-none');
 
             } else {
 
                   current--;
                   allSlides[current].classList.add('selected');
+                  allThumb[current].classList.add('t-selected');
+                  overlay[current].classList.add('t-none');
 
             }
 
